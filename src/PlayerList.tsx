6e048282ae386c523abe20c6c6c8e19508f75baf
ass <jurus32@gmail.com>
2025-07-06 06:53:02 +0700

    alter ego

--- src/PlayerList.tsx
+++ src/PlayerList.tsx
@@ -19,8 +19,8 @@ import { PlayerCard } from "./components/PlayerCardv1";
 import { getLogoCode } from "./utils/getLogoClub";
 import { CONFIG } from "./config";
 
-// import rawTopPlayers from "../public/data/alter_ego.json";
-import rawTopPlayers from "../public/data/bigetron_esports.json";
+import rawTopPlayers from "../public/data/alter_ego.json";
+// import rawTopPlayers from "../public/data/bigetron_esports.json";
 // import rawTopPlayers from "../public/data/dewa_united_esports.json";
 // import rawTopPlayers from "../public/data/geek_fam_id.json";
 // import rawTopPlayers from "../public/data/onic.json";
